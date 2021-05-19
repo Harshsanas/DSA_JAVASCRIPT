50baf952e07f0b9f9a766dc423b05be02a134d56
harshit sanas <harrysanas140@gmail.com>
2021-05-19 13:49:39 +0530

    New Sorting Algo

--- NewSortingAlgo.js
+++ NewSortingAlgo.js
@@ -1,16 +1,28 @@
 function runProgram(input) {
   var input = input.trim().split(/[\n\r]+/);
-  console.log(input);
+  //   console.log(input);
+
+  var [num, sum] = input[0].trim().split(" ").map(Number);
+  var elem = input[1].trim().split(" ").map(Number);
+
+  //   console.log(num, sum, elem);
+
+  var temp = 0;
+  for (var i = 0; i < elem.length; i++) {
+    for (var j = 0; j < elem.length - i - 1; j++) {
+      if (elem[j] % sum > elem[j + 1] % sum) {
+        temp = elem[j];
+        elem[j] = elem[j + 1];
+        elem[j + 1] = temp;
+      }
+    }
+  }
+  console.log(...elem);
 }
 
 if (process.env.USERNAME === "prasa") {
-  runProgram(`6
-rancho 45
-chatur 32
-raju 30
-farhan 28
-virus 32
-joy 45`);
+  runProgram(`5 6
+12 18 17 65 46`);
 } else {
   process.stdin.resume();
   process.stdin.setEncoding("ascii");
